f7506933e4ff3c07385024e0f2966cf0316fd277
Supreet <supreet.official2020@gmail.com>
2021-07-22 08:49:40 +0545

    homepage and bookmaker page done

---
+++ js/custom.js
@@ -0,0 +1,17 @@
+jQuery(document).ready(function ($) {
+  $('.tabtoggle-nav>li').click(function () {
+    var dataTarget = $(this).attr('data-target'),
+      parentDiv = $(this).parents('.tabtoggle'),
+      navItems = parentDiv.find('.tabtoggle-nav>li'),
+      items = parentDiv.find('.tabtoggle-item');
+
+    navItems.removeClass('active');
+    items.removeClass('active');
+    $(this).addClass('active');
+    parentDiv.find('.tabtoggle-item[data-target="' + dataTarget + '"]').addClass('active');
+  });
+
+  $('.navigation-toggle').click(function () {
+    $('.navigation').toggleClass('active');
+  });
+});
