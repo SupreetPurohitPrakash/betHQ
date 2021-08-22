bc8516f2af4a55d1d6a33be3aaba4fedbe7baf34
Supreet <supreet.official2020@gmail.com>
2021-08-22 10:38:14 +0545

    special types page

---
+++ special-types.php
@@ -0,0 +1,154 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>BET HQ</title>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css"
+        integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA=="
+        crossorigin="anonymous" />
+    <link rel="stylesheet" href="./css/bootstrap.min.css" />
+    <link rel="stylesheet" href="./css/slick.css" />
+    <link rel="stylesheet" href="./css/slick-theme.css" />
+    <link rel="stylesheet" href="./css/style.min.css" />
+
+    <link rel="shortcut icon" type="image/x-icon" href="images/favicon.png" />
+</head>
+
+<body class="page">
+
+    <?php include('includes/header.html') ?>
+
+    <div class="sitecontent">
+        <section class="section section-pageheader">
+            <div class="container">
+                <div class="pageheader">
+                    <ul>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Bookmakers</a></li>
+                        <li>Offers</li>
+                    </ul>
+                </div>
+            </div>
+        </section>
+
+        <section class="section section-pagesection pt-0">
+            <div class="container">
+                <div class="pagesection">
+                    <div class="row">
+                        <main class="pagesection-main">
+                            <div class="section-head mb-0">
+                                <h2 class="heading-alt">Best price offers</h2>
+                            </div>
+                            <div class="pagesection-div">
+                                <div class="cta-box-wrap">
+                                    <div class="cta-box">
+                                        <div class="text">
+                                            <h4 class="heading-sm">Offer</h4>
+                                            <h2 class="heading-mini">Bet £10 Get £30 in free bets - Promo Code: P30</h2>
+                                            <small>Max benefit £25,000 per customer, per day. Available on selected
+                                                races/meetings from 10:00 on day of race. If SP is greater than price
+                                                taken,
+                                                returns will be paid at SP. Customer/Player restrictions & Racing Best
+                                                Odds
+                                                Guaranteed Rules apply.</small>
+                                            <a href="#" class="link-longarrow-icon">Details</a>
+                                        </div>
+                                        <div class="box green-dark">
+                                            <img src="./images/paddypower.png" alt="">
+                                            <a href="#" class="link-box orange">Claim it now</a>
+                                        </div>
+                                    </div>
+                                    <div class="cta-box">
+                                        <div class="text">
+                                            <h4 class="heading-sm">Offer</h4>
+                                            <h2 class="heading-mini">Bet £10 Get £30 in free bets - Promo Code: P30</h2>
+                                            <small>Max benefit £25,000 per customer, per day. Available on selected
+                                                races/meetings from 10:00 on day of race. If SP is greater than price
+                                                taken,
+                                                returns will be paid at SP. Customer/Player restrictions & Racing Best
+                                                Odds
+                                                Guaranteed Rules apply.</small>
+                                            <a href="#" class="link-longarrow-icon">Details</a>
+                                        </div>
+                                        <div class="box green-light">
+                                            <img src="./images/bet-365-alt.png" alt="">
+                                            <a href="#" class="link-box orange">Claim it now</a>
+                                        </div>
+                                    </div>
+                                    <div class="cta-box">
+                                        <div class="text">
+                                            <h4 class="heading-sm">Offer</h4>
+                                            <h2 class="heading-mini">Bet £10 Get £30 in free bets - Promo Code: P30</h2>
+                                            <small>Max benefit £25,000 per customer, per day. Available on selected
+                                                races/meetings from 10:00 on day of race. If SP is greater than price
+                                                taken,
+                                                returns will be paid at SP. Customer/Player restrictions & Racing Best
+                                                Odds
+                                                Guaranteed Rules apply.</small>
+                                            <a href="#" class="link-longarrow-icon">Details</a>
+                                        </div>
+                                        <div class="box blue">
+                                            <img src="./images/william-hill-sm.png" alt="">
+                                            <a href="#" class="link-box orange">Claim it now</a>
+                                        </div>
+                                    </div>
+                                    <div class="cta-box">
+                                        <div class="text">
+                                            <h4 class="heading-sm">Offer</h4>
+                                            <h2 class="heading-mini">Bet £10 Get £30 in free bets - Promo Code: P30</h2>
+                                            <small>Max benefit £25,000 per customer, per day. Available on selected
+                                                races/meetings from 10:00 on day of race. If SP is greater than price
+                                                taken,
+                                                returns will be paid at SP. Customer/Player restrictions & Racing Best
+                                                Odds
+                                                Guaranteed Rules apply.</small>
+                                            <a href="#" class="link-longarrow-icon">Details</a>
+                                        </div>
+                                        <div class="box green-dark">
+                                            <img src="./images/paddypower.png" alt="">
+                                            <a href="#" class="link-box orange">Claim it now</a>
+                                        </div>
+                                    </div>
+                                    <div class="cta-box">
+                                        <div class="text">
+                                            <h4 class="heading-sm">Offer</h4>
+                                            <h2 class="heading-mini">Bet £10 Get £30 in free bets - Promo Code: P30</h2>
+                                            <small>Max benefit £25,000 per customer, per day. Available on selected
+                                                races/meetings from 10:00 on day of race. If SP is greater than price
+                                                taken,
+                                                returns will be paid at SP. Customer/Player restrictions & Racing Best
+                                                Odds
+                                                Guaranteed Rules apply.</small>
+                                            <a href="#" class="link-longarrow-icon">Details</a>
+                                        </div>
+                                        <div class="box green-light">
+                                            <img src="./images/bet-365-alt.png" alt="">
+                                            <a href="#" class="link-box orange">Claim it now</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </main>
+                        <side class="pagesection-side">
+                            <?php include('includes/sidebar-8.html') ?>
+                        </side>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+    </div>
+
+    <?php include('includes/footer.html') ?>
+
+    <script src="./js/jquery-3.3.1.min.js"></script>
+    <script src="./js/bootstrap.bundle.min.js"></script>
+    <script src="./js/slick.min.js"></script>
+    <script src="./js/custom.js"></script>
+</body>
+
+</html>
